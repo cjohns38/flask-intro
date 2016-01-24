e574b79ec01acca36bacc9ba4c2d0c4eca7ab435
Craig Johnson <cjohnson@cpp.com>
2016-01-24 15:32:16 -0700

    Config updates

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 
 class BaseConfig(object): 
     DEBUG = False
-    SECRET_KEY = 'my precious'
+    SECRET_KEY = '\x84{\x80K\xe6i\xbd`\xb1\xa0\x0f9\x84%s\xc9\xe7\xf5c\xb6C\xa6\xf2\xd3'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(BaseConfig):
