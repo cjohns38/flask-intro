6ba0ad72d69de834661172b529fa109d9bd0d17b
Craig Johnson <cjohnson@cpp.com>
2016-01-24 14:51:34 -0700

    updates

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash, g
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask.ext.sqlalchemy import SQLAlchemy
 from functools import wraps
-import sqlite3
+
+#import sqlite3
 
 app = Flask(__name__)
 
 app.secret_key = "my previous"
-app.database = 'sample.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 
+#cretae the sqlalchmey object
+
+db = SQLAlchemy(app)
+from models import *
 
 
 #Login required decorator 
@@ -25,14 +31,7 @@ def login_required(f):
 @app.route('/')
 @login_required
 def home():
-    posts = []
-    try:
-        g.db = connect_db()
-        cur = g.db.execute('select * from posts')
-        posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
-        g.db.close()
-    except sqlite3.OperationalError:
-        flash("You have no database")
+    posts = db.session.query(BlogPost).all()
     return render_template("index.html", posts = posts)
 
 @app.route('/welcome')
@@ -59,8 +58,8 @@ def logout():
     flash("You were just logged out")
     return redirect(url_for('welcome'))
 
-def connect_db():
-    return sqlite3.connect(app.database)
+#def connect_db():
+#    return sqlite3.connect(app.database)
 
 
 
